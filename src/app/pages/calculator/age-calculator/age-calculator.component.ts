20da610398bc423df3330f1fac97e3865e51c0b9
abdelati <chraiha1abdelati@gmail.com>
2022-04-27 03:13:11 +0000

    fix age probleme

--- src/app/pages/calculator/age-calculator/age-calculator.component.ts
+++ src/app/pages/calculator/age-calculator/age-calculator.component.ts
@@ -1,9 +1,7 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { DatePipe } from '@angular/common';
 import { DomSanitizer, Meta, SafeHtml, Title } from '@angular/platform-browser';
 import { CanonicalService } from 'src/app/services/canonical.service';
-import { ifStmt } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-age-calculator',
@@ -79,7 +77,124 @@ export class AgeCalculatorComponent implements OnInit {
   }
 
 
-  public CalculateAge(e: HTMLElement): void {
+  CalculateAge(e: HTMLElement){
+    if (this.calculeAge.valid) {
+      this.daysDiff(this.birthday.value, this.today.value);
+      this.monthsDiff(this.birthday.value, this.today.value);
+      this.fullDateDiff(this.birthday.value, this.today.value);
+      this.weeksDiff(this.birthday.value, this.today.value)
+      this.checkForm = true;
+      e.scrollIntoView({ behavior: "smooth" });
+    }else {
+      this.error = "Please check the inputs";
+    }
+  }
+
+  yearsDiff(d1: Date, d2: Date) {
+    let date1 = new Date(d1);
+    let date2 = new Date(d2);
+    let yearsDiff =  date2.getFullYear() - date1.getFullYear();
+    console.log( 'year', yearsDiff);
+    return yearsDiff;
+}
+  monthsDiff(d1: Date, d2: Date) {
+    let date1 = new Date(d1);
+    let date2 = new Date(d2);
+    let years = this.yearsDiff(d1, d2);
+    let months =(years * 12) + (date2.getMonth() - date1.getMonth()) ;
+
+    let diffDays = this.daysDiff(d1, d2);
+    if(date1.getMonth() > date2.getMonth()){
+      years = years - 1;
+    }
+    // had calcul bach n7aydo sanawat alkabissa Math.floor((29-(2022%4))/4
+    // let eleminateSanaKabisa = ((months/12)*365) + Math.floor((29-(2022%4))/4 );
+    let eleminateSanaKabisa = ((months/12)*365) + Math.floor((years-(date2.getFullYear()%4))/4 );
+    this.dayF = Math.floor( diffDays - eleminateSanaKabisa);
+    if(this.dayF < 0){
+      this.dayF = 0;
+    };
+    this.month = months 
+
+    return months;
+  }
+
+  daysDiff(d1: Date, d2: Date) {
+    let hours = this.hoursDiff(d1, d2);
+    let daysDiff = Math.floor( hours / 24 );
+    this.day = daysDiff;
+    
+    return daysDiff;
+ }
+
+ hoursDiff(d1: Date, d2:  Date) {
+  let minutes = this.minutesDiff(d1, d2);
+  let hoursDiff = Math.floor( minutes / 60 );
+  this.hours = hoursDiff;
+  
+  return hoursDiff;
+}
+
+
+minutesDiff(d1: Date, d2: Date) {
+  let seconds = this.secondsDiff(d1, d2);
+  let minutesDiff = Math.floor( seconds / 60 );
+  this.minute =  minutesDiff;
+  
+  return minutesDiff;
+}
+
+secondsDiff(d1: any, d2: any) {
+  // let millisecondDiff = d2 - d1;
+  let secDiff = Math.floor( ( d2 - d1) / 1000 );
+  this.second = secDiff;
+  
+  return secDiff;
+}
+
+weeksDiff(d1: Date, d2: Date){
+ let days = this.daysDiff(d1, d2);
+ this.week = Math.floor(days/7);
+ this.dayW = days - (this.week * 7);
+}
+
+fullDateDiff(startingDate: any, endingDate: any) {
+  var startDate = new Date(new Date(startingDate).toISOString().substr(0, 10));
+  if (!endingDate) {
+    endingDate = new Date().toISOString().substr(0, 10); // need date in YYYY-MM-DD format
+  }
+  var endDate = new Date(endingDate);
+  if (startDate > endDate) {
+    var swap = startDate;
+    startDate = endDate;
+    endDate = swap;
+  }
+  var startYear = startDate.getFullYear();
+  var february = (startYear % 4 === 0 && startYear % 100 !== 0) || startYear % 400 === 0 ? 29 : 28;
+  var daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  var yearDiff = endDate.getFullYear() - startYear;
+  var monthDiff = endDate.getMonth() - startDate.getMonth();
+  if (monthDiff < 0) {
+    yearDiff--;
+    monthDiff += 12;
+  }
+  var dayDiff = endDate.getDate() - startDate.getDate();
+  if (dayDiff < 0) {
+    if (monthDiff > 0) {
+      monthDiff--;
+    } else {
+      yearDiff--;
+      monthDiff = 11;
+    }
+    dayDiff += daysInMonth[startDate.getMonth()];
+  }
+  this.year = yearDiff;
+  this.monthF = monthDiff;
+  this.dayF = dayDiff;
+}
+
+  public CalculateAgee(e: HTMLElement): void {
     if (this.calculeAge.valid) {
       this.checkForm = true;
       e.scrollIntoView({ behavior: "smooth" });
