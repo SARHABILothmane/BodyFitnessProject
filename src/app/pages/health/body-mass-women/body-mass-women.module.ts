0176e5acf0e9d13fffa31b75712e676eba0f1665
abdelati <chraiha1abdelati@gmail.com>
2022-05-15 13:24:31 +1400

    add content to bmi women

--- src/app/pages/health/body-mass-women/body-mass-women.module.ts
+++ src/app/pages/health/body-mass-women/body-mass-women.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { BodyMassWomenComponent } from './body-mass-women.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgxJsonLdModule } from '@ngx-lite/json-ld';
+import { OtherCalcultorModule } from '../../other-calcultor/other-calculator.module';
 
 const routes: Routes = [
   { path: "", component: BodyMassWomenComponent },
@@ -18,6 +19,7 @@ const routes: Routes = [
     NbLayoutModule,
     FormBmiModule,
     NgxJsonLdModule,
+    OtherCalcultorModule,
     RouterModule.forChild(routes),
   ]
 })
