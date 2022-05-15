0c16086741c1c04d19cc5ec49847eb57d07a28a4
unknown <sarhabilothmane94@gmail.com>
2022-05-15 11:47:01 +0100

    modification ads script

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,34 +1,15 @@
-import { AfterViewInit, Component, OnInit, Input } from '@angular/core';
-import { Banner } from 'src/app/models/banner';
-import { environment } from 'src/environments/environment';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent implements AfterViewInit {
+export class BannerComponent implements OnInit {
 
-  @Input() banner!: Banner;
-  // showAd = environment.adsense.show;
   constructor() { }
 
-  // ngOnInit(): void {
-  // }
-
-  ngAfterViewInit() {
-    setTimeout(() => {
-      try {
-        // let test = (window as { [key: string]: any })["DataManager"] as string;
-        ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({
-          overlays: { bottom: true }
-        })
-        // (window['adsbygoogle'] = window['adsbygoogle'] || []).push({
-        //     overlays: {bottom: true}
-        // });
-      } catch (e) {
-        console.error(e);
-      }
-    }, 0);
+  ngOnInit(): void {
   }
+
 }
